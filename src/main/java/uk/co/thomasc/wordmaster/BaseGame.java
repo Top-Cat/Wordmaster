b26706403c3fb0d58da6ec0e847db0877e3b3e53
Top Cat <webmaster@thomasc.co.uk>
2013-06-08 00:40:06 +0100

    Implement google+ login Add items to footer bar

--- src/main/java/uk/co/thomasc/wordmaster/BaseGame.java
+++ src/main/java/uk/co/thomasc/wordmaster/BaseGame.java
@@ -3,17 +3,21 @@ package uk.co.thomasc.wordmaster;
 import android.content.Context;
 import android.graphics.Typeface;
 
+import com.google.android.gms.common.SignInButton;
+
 import uk.co.thomasc.wordmaster.util.CapsLockLimiter;
 import uk.co.thomasc.wordmaster.view.game.SwipeController;
 import uk.co.thomasc.wordmaster.view.game.SwipeListener;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -21,7 +25,7 @@ import android.widget.ImageView;
  *
  * @see SystemUiHider
  */
-public class BaseGame extends FragmentActivity {
+public class BaseGame extends BaseGameActivity implements OnClickListener {
 	
 	public static Typeface russo;
 
@@ -38,9 +42,32 @@ public class BaseGame extends FragmentActivity {
 		mPager.setAdapter(swipe);
 		mPager.setOnPageChangeListener(new SwipeListener((ImageView) findViewById(R.id.indicator)));
 		
+		SignInButton button = (SignInButton) findViewById(R.id.button_sign_in);
+		button.setOnClickListener(this);
+		button.setStyle(SignInButton.SIZE_WIDE, SignInButton.COLOR_LIGHT); // I commend anyone who can do this in XML
+		
 		EditText input = (EditText) findViewById(R.id.editText1);
 		((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(input, InputMethodManager.SHOW_FORCED);
 		getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 		input.addTextChangedListener(new CapsLockLimiter(input));
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.button_sign_in) {
+			System.out.println("click");
+			beginUserInitiatedSignIn();
+		}
+	}
+	
+	@Override
+	public void onSignInFailed() {
+		System.out.println("oh noes!");
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		((LinearLayout) findViewById(R.id.screen_login)).setVisibility(View.GONE);
+		((LinearLayout) findViewById(R.id.screen_game)).setVisibility(View.VISIBLE);
 	}
 }
