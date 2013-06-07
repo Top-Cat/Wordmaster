5f8ba9d0ea335f97ccdd5f58aa93da6a613b7979
Josh Asch <josh@josh-asch.net>
2013-06-07 19:16:20 +0100

    Getter for an actual avatar instead of the URL

--- src/main/java/uk/co/thomasc/wordmaster/Player.java
+++ src/main/java/uk/co/thomasc/wordmaster/Player.java
@@ -1,5 +1,12 @@
 package uk.co.thomasc.wordmaster;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.graphics.drawable.Drawable;
+
 public class Player {
 
 	/* Properties */
@@ -22,6 +29,17 @@ public class Player {
 	}
 	public String getAvatarURL() {
 		return avatarURL;
+	}
+	public Drawable getAvatar() {
+		try {
+			InputStream is = (InputStream) new URL(avatarURL).getContent();
+			Drawable d = Drawable.createFromStream(is, "player avatar");
+			return d;
+		} catch (MalformedURLException ex) {
+			return null;
+		} catch (IOException ex) {
+			return null;
+		}
 	}
 	
 }
