8f90eab0f148ba6b67cdd0ac2d85d9660e79c3ea
Top Cat <webmaster@thomasc.co.uk>
2013-06-10 00:57:03 +0100

    Switch to spinner on clicking refresh Added "Wordmaster" header text

--- src/main/java/uk/co/thomasc/wordmaster/BaseGame.java
+++ src/main/java/uk/co/thomasc/wordmaster/BaseGame.java
@@ -28,7 +28,7 @@ public class BaseGame extends BaseGameActivity implements OnClickListener {
 		
 		russo = Typeface.createFromAsset(getAssets(), "fonts/Russo_One.ttf");
 
-		setContentView(R.layout.activity_fullscreen);
+		setContentView(R.layout.menu_screen);
 		
 		SignInButton button = (SignInButton) findViewById(R.id.button_sign_in);
 		button.setOnClickListener(this);
@@ -40,6 +40,10 @@ public class BaseGame extends BaseGameActivity implements OnClickListener {
 		if (v.getId() == R.id.button_sign_in) {
 			//beginUserInitiatedSignIn(); For now we skip this
 			onSignInSucceeded();
+		} else if (v.getId() == R.id.refresh) {
+			findViewById(R.id.refresh).setVisibility(View.GONE);
+			findViewById(R.id.refresh_progress).setVisibility(View.VISIBLE);
+			//TODO: Populate menu feed
 		}
 	}
 	
@@ -53,6 +57,9 @@ public class BaseGame extends BaseGameActivity implements OnClickListener {
 		findViewById(R.id.button_sign_in).setVisibility(View.GONE);
 		findViewById(R.id.whysignin).setVisibility(View.GONE);
 		findViewById(R.id.main_feed).setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 1F));
+		
+		// Enable UI
+		findViewById(R.id.refresh).setOnClickListener(this);
 		
 		//TODO: Populate menu feed
 		
