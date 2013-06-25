413e0d3faf9307f7a0d33eed856e1a9a92e2c50c
Top Cat <webmaster@thomasc.co.uk>
2013-06-25 20:01:25 +0100

    Layout for user's word choice... with BLUE! Also smaller text for thiner aspect screens

--- src/main/java/uk/co/thomasc/wordmaster/view/game/GameAdapter.java
+++ src/main/java/uk/co/thomasc/wordmaster/view/game/GameAdapter.java
@@ -53,39 +53,34 @@ public class GameAdapter extends ArrayAdapter<Turn> {
 		User user = item.getUser();
 		boolean isPlayer = (user.getPlusID().equals(((BaseGame) act).getUserId()));
 		final boolean winningTurn = (item.getCorrectLetters() == 4);
-		int viewId = isPlayer ? (winningTurn ? R.layout.game_row_win : R.layout.game_row_big) : (winningTurn ? R.layout.game_row_lose : R.layout.game_row_small);
-		if (item.getTurnNum() == 0) {
-			viewId = R.layout.game_row_new_round;
-		}
+		int viewId = item.getTurnNum() == 0 ? R.layout.game_row_word : (isPlayer ? (winningTurn ? R.layout.game_row_win : R.layout.game_row_big) : (winningTurn ? R.layout.game_row_lose : R.layout.game_row_small));
 		
 		if (view == null || view.getId() != viewId) {
 			LayoutInflater vi = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			view = vi.inflate(viewId, null);
 		}
 		
-		if (isPlayer) {
-			if (item.getTurnNum() == 0) {
-				((TextView) view.findViewById(R.id.guess)).setText("Your word for this round is " + item.getGuess().toUpperCase(Locale.ENGLISH));
-			} else {
-				((TextView) view.findViewById(R.id.guess)).setText(item.getGuess().toUpperCase(Locale.ENGLISH));
-			}
+		if (item.getTurnNum() == 0) {
+			((TextView) view.findViewById(R.id.guess)).setText("Your word is " + item.getGuess());
 		} else {
-			final TextView txtview = (TextView) view.findViewById(R.id.guess);
-			user.listenForLoad(new NameLoadedListener() {
-				@Override
-				public void onNameLoaded(String name) {
-					String firstName = name.substring(0, name.indexOf(' '));
-					if (winningTurn) {
-						txtview.setText(firstName + " guessed " + item.getGuess().toUpperCase(Locale.ENGLISH) +
-								"\n" + firstName + "'s word was " + item.getOpponentWord().toLowerCase(Locale.ENGLISH));
-					} else {
-						txtview.setText(firstName + " guessed " + item.getGuess().toUpperCase(Locale.ENGLISH));
+			if (isPlayer) {
+				((TextView) view.findViewById(R.id.guess)).setText(item.getGuess().toUpperCase(Locale.ENGLISH));
+			} else {
+				final TextView txtview = (TextView) view.findViewById(R.id.guess);
+				user.listenForLoad(new NameLoadedListener() {
+					@Override
+					public void onNameLoaded(String name) {
+						String firstName = name.substring(0, name.indexOf(' '));
+						if (winningTurn) {
+							txtview.setText(firstName + " guessed " + item.getGuess().toUpperCase(Locale.ENGLISH) +
+									"\n" + firstName + "'s word was " + item.getOpponentWord().toLowerCase(Locale.ENGLISH));
+						} else {
+							txtview.setText(firstName + " guessed " + item.getGuess().toUpperCase(Locale.ENGLISH));
+						}
 					}
-				}
-			});
-		}
-		
-		if (item.getTurnNum() != 0) {
+				});
+			}
+			
 			((TimeSinceText) view.findViewById(R.id.time)).setTimestamp(item.getUnixTimestamp());
 			
 			int goldPegs = item.getCorrectLetters();
@@ -96,7 +91,7 @@ public class GameAdapter extends ArrayAdapter<Turn> {
 			Drawable silver = res.getDrawable(R.drawable.silverpeg);
 			Drawable white = res.getDrawable(R.drawable.whitepeg);
 			
-			if (! winningTurn) {
+			if (!winningTurn) {
 				int[] pegs = { R.id.peg0, R.id.peg1, R.id.peg2, R.id.peg3 };
 				for (int peg : pegs) {
 					if (goldPegs > 0) {
