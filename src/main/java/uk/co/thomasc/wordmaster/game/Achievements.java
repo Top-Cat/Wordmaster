c1a13be33e327858fe209b947dc7567df30f98f8
Top Cat <webmaster@thomasc.co.uk>
2013-06-30 17:33:05 +0100

    More achievements

--- src/main/java/uk/co/thomasc/wordmaster/game/Achievements.java
+++ src/main/java/uk/co/thomasc/wordmaster/game/Achievements.java
@@ -6,6 +6,9 @@ public enum Achievements {
 	IDONTHATEYOU(4, "CgkIj_Ku7KQVEAIQAw"),
 	LUCKY(5, "CgkIj_Ku7KQVEAIQBA"),
 	PARTICIPANT(1, new String[] {"CgkIj_Ku7KQVEAIQBQ", "CgkIj_Ku7KQVEAIQBg", "CgkIj_Ku7KQVEAIQCA", "CgkIj_Ku7KQVEAIQBw"}, true),
+	GOLDPEGS(2, new String[] {"CgkIj_Ku7KQVEAIQCQ", "CgkIj_Ku7KQVEAIQCg", "CgkIj_Ku7KQVEAIQCw", "CgkIj_Ku7KQVEAIQDA"}, true),
+	SILVERPEGS(3, new String[] {"CgkIj_Ku7KQVEAIQDg", "CgkIj_Ku7KQVEAIQDw", "CgkIj_Ku7KQVEAIQEA", "CgkIj_Ku7KQVEAIQEQ"}, true),
+	NEMESIS(6, "CgkIj_Ku7KQVEAIQDQ"),
 	;
 	
 	private int sid;
